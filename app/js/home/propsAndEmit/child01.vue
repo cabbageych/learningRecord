e8f70eb40bfbf7ec62596feb19fcb41292cc9c85
cabbageych <1109685259@qq.com>
2019-04-09 12:53:04 +0800

    update

---
+++ app/js/home/propsAndEmit/child01.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    传值给父组件:
+    <input type="text" v-model="toParent">
+    {{toParent}}
+    <button @click="toP">submit</button><br>
+    来自父组件的值:{{inputName}}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      toParent: "emmm"
+    };
+  },
+  // 接受父组件的值
+  props: {
+    inputName: String,
+    required: true
+  },
+  methods: {
+    toP: function() {
+      this.$emit("fromChild", this.toParent);
+    }
+  }
+};
+</script>
---
+++ app/js/home/propsAndEmit/parent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    父组件:
+    <span>childValue:{{childValue}}</span>
+    <child :inputName="name" v-on:fromChild="showValueC"></child>
+    父组件传值给子组件:name:<input v-model="name">{{name}}
+  </div>
+</template>
+<script>
+import child from "./child01.vue";
+export default {
+  components: {
+    child
+  },
+  data() {
+    return {
+      name: "toChild",
+      childValue: "waiting..."
+    };
+  },
+  methods: {
+    showValueC: function(tempstr) {
+      this.childValue = tempstr;
+    }
+  }
+};
+</script>
