3ca8728fc74060f6bdd642f51dd48349e03d4a38
cabbageych <1109685259@qq.com>
2019-04-16 10:13:43 +0800

    update

--- ajaxWithNode/serverPost.js
+++ ajaxWithNode/serverPost.js
@@ -1,19 +1,29 @@
 'use strict'
-
 var http = require('http');
+var url = require('url');
 var querystring = require('querystring');
 
+var server = http.createServer((req, response) => {
 
-var body = '';
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
-    request.on('data', function (chunk) {
-        body += chunk;
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "true");
+    response.setHeader("Access-Control-Allow-Methods", "*");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type,Access-Token");
+    response.setHeader("Access-Control-Expose-Headers", "*");
+    //面对post提交，nodejs用监听的写法处理
+    //data是一个事件，表示一个小包传输完毕后做的事情
+    var result = "";
+    req.on("data", function (chunk) {
+        console.log("小包传输完毕");
+        result += chunk;
     });
-    request.on('end',function(){
-        body = querystring.parse(body);
-        console.log(body);
+    //end也是一个事件，表示所有的包传输完毕
+    req.on("end", function () {
+        //所有的包传输完毕
+        result = querystring.parse(result);
+        console.log(result.name);
     });
-    //console.log(body.name);
+    response.write("hello node");
+    response.end();
 });
 server.listen(8080);
