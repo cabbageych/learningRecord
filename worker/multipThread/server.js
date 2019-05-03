73993df79336e3fabee0ec24a29f0fd09b1dd804
cabbageych <1109685259@qq.com>
2019-05-03 21:35:57 +0800

    update

---
+++ worker/multipThread/server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var server = http.createServer(function(request,response){
+    var pathname = url.parse(request.url).pathname;
+    fs.readFile(pathname.substr(1),function(err,data){
+        if(err){
+            console.log(err);
+            response.writeHead(404,{"Content-Type":"text/html"});
+        }else{
+            response.writeHead(200,{"Content-Type":"text/html"});
+            response.write(data.toString());
+        }
+        response.end();
+    });
+});
+
+server.listen(8080);
