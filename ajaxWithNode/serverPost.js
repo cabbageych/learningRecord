a79c98a77eab3adfe49b62613c2c967da581beaa
cabbageych <1109685259@qq.com>
2019-05-06 10:38:26 +0800

    update

--- ajaxWithNode/serverPost.js
+++ ajaxWithNode/serverPost.js
@@ -6,7 +6,6 @@ var querystring = require('querystring');
 
 var arr = [];
 var server = http.createServer((request, response) => {
-
     response.setHeader("Access-Control-Allow-Origin", "*");
     //response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "*");
@@ -14,16 +13,24 @@ var server = http.createServer((request, response) => {
     //response.setHeader("Access-Control-Expose-Headers", "*");
     //面对post提交，nodejs用监听的写法处理
     //data是一个事件，表示一个小包传输完毕后做的事情
-    var temp;
-    request.on('data', function (message) {
-        console.log(message);
-        arr.push(eval('(' + message + ')'));
-    });
-    request.on('end', function () {
-        console.log(arr[0]);
-    });
-    response.write("hello node");
-    response.end();
+    
+    //console.log(request.method);
+    if (request.method == "POST") {
+        request.on('data', function (message) {
+            //console.log(message);
+            arr.push(JSON.parse(message));
+            //arr.push(eval('(' + message + ')'));
+        });
+        request.on('end', function () {
+            console.log(arr[0].name);
+            response.write("connection end.");
+            response.end();
+        });
+    }
+    if (request.method == 'OPTIONS') {
+        response.end();
+    }
+
 });
 
 
