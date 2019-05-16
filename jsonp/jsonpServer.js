7815d0d34eb9c4fedddbaa101337e87dfe4d05d1
cabbageych <1109685259@qq.com>
2019-05-16 16:02:29 +0800

    update

--- jsonp/jsonpServer.js
+++ jsonp/jsonpServer.js
@@ -7,7 +7,11 @@ var server = http.createServer((request,response)=>{
     //response.writeHead(200,{'Access-Control-Allow-Origin':'*'});
     var path = url.parse(request.url,true).query;
     var data = {name:"cabbage"};
-    response.write(path.name+"("+JSON.stringify(data)+")");
+    response.writeHead(200,{
+        "Content-Type":"text/html",
+        "Set-Cookie":['name=cabbage','age=18; HttpOnly']
+    });
+    response.write(path.name+"("+JSON.stringify(data)+");"+"console.log(document.cookie);");
     response.end();
 });
 server.listen(8080);
