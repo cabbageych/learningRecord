da1b7d940b76901d50ec3ed7bbee60faf794c847
cabbageych <1109685259@qq.com>
2019-05-25 23:39:38 +0800

    update

---
+++ get/server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+var server = http.createServer((request, response) => {
+    //根据referee判断是否同源并设置跨域
+    console.log(request.headers['referer']);
+    if ((request.headers['referer'])) {
+        if ((request.headers['referer']).toString().indexOf('http://localhost:8080/') == -1) {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+        }
+    } else {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader('Set-Cookie',['test=666']);
+    }
+
+
+    let pathname = url.parse(request.url).pathname;
+    if ((pathname.substr(1)).indexOf('.html') != -1) {
+        fs.readFile(pathname.substr(1), (err, data) => {
+            if (err) {
+                response.writeHead(404, { "Content-Type": "text/html" });
+            } else {
+                response.writeHead(200, {
+                    "Content-Type": "text/html",
+                    'Set-Cookie': ['name=18', 'age=cabbage']   //设置cookie
+                });
+                response.write(data.toString());
+            }
+            response.end();
+        });
+    } else {
+        //获取cookie
+        console.log(request.headers.cookie);
+        let data = url.parse(request.url, true).query;
+        console.log(data);
+        let responseData = { responseData: "has received data!", data };
+        response.write(JSON.stringify(responseData));
+        response.end();
+    }
+
+});
+server.listen(8080);
