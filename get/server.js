3a01d48c28ed8f0818cef5bf0896f46e6247000b
cabbageych <1109685259@qq.com>
2019-05-26 11:12:32 +0800

    update

--- get/server.js
+++ get/server.js
@@ -4,16 +4,15 @@ const url = require('url');
 
 var server = http.createServer((request, response) => {
     //根据referee判断是否同源并设置跨域
-    console.log(request.headers['referer']);
-    if ((request.headers['referer'])) {
-        if ((request.headers['referer']).toString().indexOf('http://localhost:8080/') == -1) {
-            response.setHeader("Access-Control-Allow-Origin", "*");
-        }
-    } else {
-        response.setHeader("Access-Control-Allow-Origin", "*");
+    //console.log(request.headers['referer']);
+    //console.log(request.headers);
+
+    if(request.headers.host == 'localhost:8080'){
+        //直接浏览器打开html文件时设置相应头信息如下
+        response.setHeader('Access-Control-Allow-Origin',"null");
+        response.setHeader("Access-Control-Allow-Credentials","true");
         response.setHeader('Set-Cookie',['test=666']);
     }
-
 
     let pathname = url.parse(request.url).pathname;
     if ((pathname.substr(1)).indexOf('.html') != -1) {
--- post/server.js
+++ post/server.js
@@ -2,8 +2,33 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-var server = http.createServer((request,response)=>{
+var dataContainer = [];
 
+var server = http.createServer((request,response)=>{
+    //response.setHeader("Access-Control-Allow-Origin","*");
+    if(request.method == 'GET'){
+        let pathname = url.parse(request.url).pathname;
+        fs.readFile(pathname.substr(1),(err,data)=>{
+            if(err){
+                response.writeHead(404,{"Content-Type":"text/html"});
+            }else{
+                response.writeHead(200,{"Content-Type":"text/html"});
+                response.write(data.toString());
+            }
+            response.end();
+        })
+    }else if(request.method == 'POST'){
+        request.on('data',function(message){
+            dataContainer.push(JSON.parse(message));
+        });
+        request.on('end',function(){
+            console.log(dataContainer);
+            response.write('connection end...');
+            response.end();
+        })
+    }else{
+        response.end();
+    }
 });
 
 server.listen(8080);
