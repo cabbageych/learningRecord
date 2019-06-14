7d13c82cb2dda6dc3fa4f9933ccf342c21a137ff
cabbageych <1109685259@qq.com>
2019-06-14 14:35:38 +0800

    update

--- quickSort.js
+++ quickSort.js
@@ -1,15 +1,25 @@
 let arr = [11, 23, 55, 2, 66, 8];
 function quickSort(arr, start, end) {
-    if (end >= start) {
+    if (end <= start) {
         return;
     }
     let left = start;
     let right = end;
     let temp = arr[left];
     while (left < right) {
-        while(arr[right]>temp&&left<right){
+        while (arr[right] > temp && left < right) {
             --right;
+        }
+        arr[left] = arr[right];
+        while (arr[left] < temp && left < right) {
+            ++left;
         }
         arr[right] = arr[left];
     }
+    arr[right] = temp;
+    quickSort(arr, start, right - 1);
+    quickSort(arr, right + 1, end);
 }
+console.log(arr);
+quickSort(arr, 0, arr.length - 1);
+console.log(arr);
