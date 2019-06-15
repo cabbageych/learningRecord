b282f0239f74c9978fdcfb5ae30fbfa51dda4b2a
cabbageych <1109685259@qq.com>
2019-06-15 18:33:20 +0800

    update

--- binarySearch.js
+++ binarySearch.js
@@ -1,19 +1,24 @@
-'use strict'
-var a = [1,3,12,22,34,50];
-function find(arr,l,r,target){
-    var mid = (l+r)/2;
+let arr = [];
+arr[0] = Math.floor(Math.random() * 20);
+for (let i = 1; i < 20; i++) {
+    arr[i] = arr[i - 1] + Math.floor(Math.random() * 10);
+}
+let pos = Math.floor(Math.random() * 20);
+let target = arr[pos];
+console.log("pos: " + pos + ", target: " + target);
+console.log(arr);
+
+function binarySearch(arr, target, left, right) {
+    let mid = (left + right) / 2;
     mid = Math.floor(mid);
-    if(arr[mid] == target){
+    if (arr[mid] == target) {
         return mid;
     }
-    if(l == r){
-        console.log("no found!");
-        return;
-    }
-    if(arr[mid] > target){
-        return find(arr,l,mid-1,target);
-    }else{
-        return find(arr,mid+1,r,target);
+    if (arr[mid] > target) {
+        return binarySearch(arr, target, left, mid - 1);
+    } else {
+        return binarySearch(arr, target, mid + 1, right);
     }
 }
-console.log(find(a,0,a.length-1,3));
+console.log("pos:");
+console.log(binarySearch(arr, target, 0, arr.length - 1));
