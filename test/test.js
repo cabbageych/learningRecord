4e9e43a08f135ddf4322d428f52b07f69347cadd
cabbageych <1109685259@qq.com>
2019-07-07 23:29:16 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,1 +1,27 @@
-console.log(0.1+0.7);
+let arr = [];
+let obj = {
+    name: 'cabbage',
+    age: 20,
+    name: 'cabbage',
+    inner: {
+        age: 20
+    }
+}
+
+console.log(Object.keys(obj));
+
+function isCircle(obj) {
+    for (let i in obj) {
+        if (typeof obj[i] == 'object') {
+            if (arr.indexOf(obj[i]) != -1) {
+                return false;
+            } else {
+                arr.push(obj[i]);
+                return isCircle(obj[i]);
+            }
+        }
+    }
+    return true;
+}
+
+console.log(isCircle(obj));
