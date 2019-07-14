cca82964c70a5eb1989b913a32ffa9fb7fb46f6c
cabbageych <1109685259@qq.com>
2019-07-14 14:27:46 +0800

    update

--- palindromicSequence.js
+++ palindromicSequence.js
@@ -10,20 +10,53 @@ let str;
 
 function palindromic() {
     let arr = str.split("");
-    let max = 0;
+    let max = 0, tempMax = 0;
+    let pos, tempPos;
+    let ansStr, tempAnsStr;
     for (let i = 0; i < arr.length; i++) {
         let len = 0;
+        let flag = true;
+        tempMax = 0;
+        tempAnsStr = [];
         while (true) {
-            if (i == len) {
-                return;
+            if (i - len == 0) {
+                break;
+            }
+            if (arr[i - len] == arr[i + len]) {
+                if (arr[i] != '#' && flag) {
+                    tempAnsStr.push(arr[i]);
+                    flag = false;
+                    ++len;
+                } else {
+                    if (arr[i - len] != '#') {
+                        tempAnsStr.push(arr[i - len]);
+                        tempAnsStr.unshift(arr[i + len]);
+                    }
+                    ++len;
+                    ++tempMax;
+                    tempPos = i;
+                }
+            } else {
+                break;
             }
         }
+        max = max > tempMax ? max : tempMax;
+        pos = max > tempMax ? pos : tempPos;
+        ansStr = max > tempMax ? ansStr : tempAnsStr;
     }
+    console.log(max, pos, ansStr);
 }
 
 input.on('line', function (line) {
     str = line;
-    console.log(palindromic());
+    str = line.split("");
+    str = str.join("#");
+    str = str.split('');
+    str.unshift('#');
+    str.push('#');
+    str = str.join("");
+    console.log(str);
+    palindromic();
 });
 
 input.on('close', function () {
