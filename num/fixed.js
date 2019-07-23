3ef930cd4bd029f11e5280f035f476bc18029c2c
cabbageych <1109685259@qq.com>
2019-07-23 22:08:10 +0800

    update

---
+++ num/fixed.js
@@ -0,0 +1,12 @@
+let a = 1.22;
+let b = 100;
+let c = 1.55;
+let d = 1.56;
+let e = 1.54;
+let f = '1.45';
+console.log(a.toFixed(0),a);
+console.log(b.toFixed(3));
+
+//toFixed遵循四舍五入法则
+console.log(e.toFixed(1),c.toFixed(1),d.toFixed(1));
+//console.log(f.toFixed(1)); //error
