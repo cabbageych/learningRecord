b2b1cad37b04e95004c65d164fe24d46ee4fcdd0
cabbageych <1109685259@qq.com>
2019-08-17 21:02:55 +0800

    let const var

--- variable/const.js
+++ variable/const.js
@@ -1,4 +1,11 @@
 'use strict'
 //const 不存在变量提升
-console.log(a);  //a is not defined
+//console.log(a);  //a is not defined
 const a = 666;
+
+//const a = 777;
+
+const c = {};
+c.name = 'cabbage';
+
+//const b;
