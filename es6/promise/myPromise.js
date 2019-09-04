47d137a1c16c46b7d06350bf24fb0889883f1bf6
cabbageych <1109685259@qq.com>
2019-09-04 11:28:29 +0800

    update

--- es6/promise/myPromise.js
+++ es6/promise/myPromise.js
@@ -6,12 +6,14 @@ function myPromise(fn) {
     this.then = function (cb) {
         console.log('enter then!');
         _this.resolveCallback.push(cb);
+        return _this;
     }
     this.resolve = function (arg) {
-        _this.resolveArg = arg;
         setTimeout(() => {
-            _this.resolveCallback[0](_this.resolveArg);
-        });
+            _this.resolveCallback.forEach(el => {
+                el(arg);
+            });
+        })
     }
     fn(_this.resolve);
 }
@@ -22,4 +24,30 @@ new myPromise((resolve) => {
     resolve('enter resolve!');
 }).then(val => {
     console.log(val);
+})/*.then(val => {
+    console.log('then02');
+});*/
+
+
+/*
+console.log('-------------')
+
+let a = new Promise((resolve,reject)=>{
+    console.log('promise01');
+    resolve("enter 02")
 });
+a.then(val=>{
+    console.log(val);
+    return new Promise(resolve=>{
+        console.log("promise02")
+        resolve('enter 02')
+    })
+}).then(val=>{
+    console.log(val);
+})*/
+/**
+promise01
+enter 02
+promise02
+enter 02
+ */
