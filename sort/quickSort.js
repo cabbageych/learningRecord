ff866ef406b305a0fed6d3494f3868fc6b6f5776
cabbageych <1109685259@qq.com>
2019-09-14 23:48:27 +0800

    update

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -1,24 +1,24 @@
 let arr = [11, 23, 55, 2, 66, 8];
 function quickSort(arr, start, end) {
-    if (end <= start) {
+    if (start >= end) {
         return;
     }
     let left = start;
     let right = end;
-    let temp = arr[left];
+    let base = arr[start];
     while (left < right) {
-        while (arr[right] > temp && left < right) {
+        while (arr[right] > base && left < right) {
             --right;
         }
         arr[left] = arr[right];
-        while (arr[left] < temp && left < right) {
+        while (arr[left] < base && left < right) {
             ++left;
         }
         arr[right] = arr[left];
     }
-    arr[right] = temp;
-    quickSort(arr, start, right - 1);
-    quickSort(arr, right + 1, end);
+    arr[left] = base;
+    quickSort(arr, start, left - 1);
+    quickSort(arr, left + 1, end);
 }
 console.log(arr);
 quickSort(arr, 0, arr.length - 1);
